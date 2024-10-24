88bf5ef5886e7bca57e853ded7df47a5c0c539c0
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-10-24 13:21:02 -0400

    Add render function to get fetched data and show the city and temp on the page

---
+++ src/render.js
@@ -0,0 +1,20 @@
+import {url} from "./api.js";
+import{hours} from "./hourlyWeather.js";
+
+export async function render(){
+    let response=await fetch(url);
+    if(!response.status==200){
+        throw new Error(`Errror! ${response.status}`)
+    }
+    let json = await response.json();
+    const temps=[];
+    const arr=[1,4,7,10,13,16,19,22 ];
+arr.forEach(i=>{
+    temps.push(json.days[0].hours[i]);
+});
+console.log(temps);
+hours(temps);
+    document.querySelector("h3").innerText = `City: ${json.address}`;
+    document.querySelector("p").innerText=`Condition: ${json.currentConditions.conditions}`;
+    document.querySelector("h4").innerText=`Temperature: ${json.currentConditions.temp}`;
+}
