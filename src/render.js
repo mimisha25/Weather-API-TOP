233fffd427468b1fa6e6bda4c10324252f6d4316
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-10-26 15:50:57 -0400

    Create array with hours to show hourly weather information

--- src/render.js
+++ src/render.js
@@ -16,6 +16,10 @@ async function day(url){
     }
 
     let json = await response.json();
+    const temps=[];
+    const arr=[1,4,7,10,13,16,19,22 ];
+    arr.forEach(i=>temps.push(json.days[0].hours[i]));
+    hours(temps);
 
     document.querySelector("#day h3").innerText = `City: ${json.address}`;
     document.querySelector("p").innerText=`Condition: ${json.currentConditions.conditions}`;
