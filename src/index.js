edebe1eea98e0403b1d736cf9252a3b8a3ecc273
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-10-26 15:44:54 -0400

    Add welcome text with todays time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
- import "./styles.css";
- import {url} from "./api.js";
- import {render} from "./render.js";
+import "./styles.css";
+import {url} from "./api.js";
+import {render} from "./render.js";
 
 const input = document.querySelector("input");
 const form = document.querySelector("form");
-
-render();
+const welcome = document.querySelector(".date");
+const hi = document.createElement("h3");
+const todayDate = document.createElement("div");
+hi.innerText="Hi, There"
+let date = new Date();
+todayDate.innerText=`Today is: ${date.toString().slice(4, 15)}`;
+welcome.append(hi);
+welcome.append(todayDate);
+render(url);
 
 input.addEventListener("keyup", (e)=>{
 e.preventDefault();
@@ -15,12 +22,12 @@ url.pathname=pathArray.join("/");
 })
 
 form.addEventListener("submit", async (e)=>{
-     e.preventDefault();
-    try{
+    e.preventDefault();
+   try{
 r();
-    }catch (e){
-            alert(`Errrror for total: ${e}`)
-    }
+   }catch (e){
+           alert(`Errrror for total: ${e}`)
+   }
 });
 
 
