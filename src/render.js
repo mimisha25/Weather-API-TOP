520937e62dad9e2b3aff15fb59967c3cb692392f
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-10-26 15:50:17 -0400

    Add function that fetches url and recieves the data and display them as city, temp, weather condition

--- src/render.js
+++ src/render.js
@@ -6,20 +6,18 @@ export function render(url){
     week(url);
 }
 
-export async function render(){
-    let response=await fetch(url);
+async function day(url){
+    let pathArray = url.pathname.split("/");
+    pathArray[pathArray.length - 1] = "today";
+    url.pathname=pathArray.join("/");
+     let response=await fetch(url);
     if(!response.status==200){
         throw new Error(`Errror! ${response.status}`)
     }
+
     let json = await response.json();
-    const temps=[];
-    const arr=[1,4,7,10,13,16,19,22 ];
-arr.forEach(i=>{
-    temps.push(json.days[0].hours[i]);
-});
-console.log(temps);
-hours(temps);
-    document.querySelector("h3").innerText = `City: ${json.address}`;
+
+    document.querySelector("#day h3").innerText = `City: ${json.address}`;
     document.querySelector("p").innerText=`Condition: ${json.currentConditions.conditions}`;
-    document.querySelector("h4").innerText=`Temperature: ${json.currentConditions.temp}`;
+    document.querySelector("h4").innerText=`Temperature: ${json.currentConditions.temp} F`;
 }
