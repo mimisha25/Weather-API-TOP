ac66a44f6e25574ccf5fdc1e768b918317638340
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-10-29 16:02:10 -0400

    Feat function which shows only short version of date and shows the day of week

--- src/weeklyWeather.js
+++ src/weeklyWeather.js
@@ -7,3 +7,39 @@ divs.classList.add("weekly");
 week.appendChild(h3);
 week.appendChild(divs);
 
+export function weeks(json){
+    divs.innerHTML="";
+        for(let i=0; i<7; i++){
+            const div = document.createElement("div");
+            div.classList.add("weekDiv");
+            const h51=document.createElement("h5");
+            let shortDate=json.days[i].datetime;
+            h51.innerText=convert(shortDate).slice(4, 10);
+    
+            const h5=document.createElement("h5");
+            h5.innerText=convert(shortDate).slice(0, 4);
+          
+            const img=document.createElement("img");
+            img.classList.add("imgIconW");
+            img.setAttribute("src", "");
+             let weatherCondition = json.days[i].icon;
+            icon(weatherCondition, div, img);
+    
+            const h6=document.createElement("h5");
+             h6.innerText=`${json.days[i].temp} F`;
+         
+            div.appendChild(h5);
+            div.appendChild(h51);
+            div.appendChild(h6);
+            divs.appendChild(div);
+        }
+    }
+    
+    function convert(shortDate){
+        const year = parseInt(shortDate.slice(0, 4));
+        const month =parseInt(shortDate.slice(5, 7))-1;
+        const day = parseInt(shortDate.slice(8,10));
+       let date = new Date(year, month, day);
+       return date.toString();
+    
+    }
